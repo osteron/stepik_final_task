be46b8e5ff19e7309cc1e188066b80d6e1e01ba9
Osteron <dimaslepyan@gmail.com>
2023-01-15 18:53:46 +0300

    refactor test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,3 @@
-import time
-
 from .pages.product_page import ProductPage
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
@@ -11,4 +9,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_book_to_basket()
     page.solve_quiz_and_get_code()
     page.check_book_is_added_to_basket()
-    # time.sleep(360)
