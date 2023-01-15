35a2e94618252e382ceedd5d7167e364baa07261
Osteron <dimaslepyan@gmail.com>
2023-01-15 17:02:18 +0300

    refactoring

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,14 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
 # def test_add_to_cart(browser):
 #     page = ProductPage(url="", browser)   # инициализируем объект Page Object
 #     page.open()                           # открываем страницу в браузере
